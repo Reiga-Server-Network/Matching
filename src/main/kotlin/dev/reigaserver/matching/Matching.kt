50270666de58102fcc61ec455c91cab2646a3121
Ign1s-Reiga <remrem4862@gmail.com>
2026-05-24 17:39:05 +0900

    Initial commit

---
+++ src/main/kotlin/dev/reigaserver/matching/Matching.kt
@@ -0,0 +1,14 @@
+package dev.reigaserver.matching
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Matching : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
